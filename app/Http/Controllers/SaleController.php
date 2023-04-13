12d2ed3bbb97c2392e7c464a7c7ffe2a94c3d45c
fouad <fouadwork2@gmail.com>
2023-04-13 20:30:03 +0000

    add sales Controller

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -99,7 +99,7 @@ class SaleController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Show the form for editing the specified resource.///
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
